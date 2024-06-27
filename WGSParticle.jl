b130746ab3ad3e213048b23fd646f2f4d5c45f7c
aman <amansantsingh@hotmail.com>
2024-06-27 16:36:22 +0200

    Minor changes.

--- WGSParticle.jl
+++ WGSParticle.jl
@@ -1,15 +1,7 @@
-#### Catalyst particle balance for WGS reactor model ####
+##### Catalyst particle balance for WGS reactor model #####
 
-### Define functions ###
 using Pkg
 Pkg.activate("WGS")
-# Pkg.add("ModelingToolkit")
-# Pkg.add("Symbolics")
-# Pkg.add("OrdinaryDiffEq")
-# Pkg.add("DomainSets")
-# Pkg.add("MethodOfLines"))
-using Symbolics
-using ModelingToolkit
 
 ### Defining naming conventions and numeration ###
 
@@ -20,10 +12,9 @@ using ModelingToolkit
 # 4 - H2O
 # 5 - N2
 
-### Listing some assumptions made for certain elements of the code ###
-# 1. T_c is kept constant, as we are only interested in the evolution of C_c_i
+#### Define functions for physical properties ####
 
-### Define functions for physical properties ###
+using Symbolics
 
 ## Diffusivity ##
 
@@ -209,6 +200,12 @@ function k_c_i_func(T_c, P_c, R, C_i, D_i_m)
     return k_c_i
 end
 
+#### Particle balance WGS reactor model ####
+
+### Listing some assumptions made for this part of the code ###
+# 1. T_c is kept constant, as we are only interested in the evolution of C_c_i
+# 2. P_c is kept constant
+
 ### PDE system ###
 
 # Reactor and catalyst properties
@@ -225,6 +222,7 @@ d_cat_val = 5904 # [kg/m^3]
 # Temperature and pressure values (constant)
 T_val = 450.0 # [K]
 T_c_val = 300.0 # [K]
+P_c_val = 1.3 # [atm]
 
 # Parameters for inlet and initial conditions
 F_0 = 10 # [mol/h]
@@ -232,20 +230,22 @@ R_atmm3 = 8.2057e-5 # [m3 atm/mol K]
 
 # Inlet conditions
 T_in = T_val # [K] 200 C
-P_in = 1.3 # [atm]
+P_in = P_c_val # [atm]
 y_0 = [0.2749, 0.1198, 0.2686, 0.3165, 0.0202]
 V_flow_0 = (F_0 * R_atmm3 * T_in) / P_in # [m3/h]
 C_i_val = y_0 * (F_0 / V_flow_0) # [mol/m3]
 
 # Initial conditions
 T_init = T_c_val # [K] 200 C
-P_init = 1.3 # [atm]
+P_init = P_c_val # [atm]
 y_init = [0.0, 0.0, 0.0, 0.0, 1.0]
 V_flow_init = (F_0 * R_atmm3 * T_init) / P_init # [m3/h]
 C_c_i_init = y_init * (F_0 / V_flow_init) # [mol/m3]
 
+using ModelingToolkit
+
 ## Parameters ##
-@parameters t r T_c R θ τ rad d_cat C_i[1:5]
+@parameters t r T_c R θ τ rad d_cat P_c C_i[1:5]
 
 ## Differential ##
 Dt = Differential(t)
@@ -253,7 +253,7 @@ Dr = Differential(r)
 Drr = Differential(r)^2
 
 ## Variables ##
-@variables C_c_1(..) C_c_2(..) C_c_3(..) C_c_4(..) C_c_5(..) P_c(t, r) D_1_m(t, r) D_2_m(t, r) D_3_m(t, r) D_4_m(t, r) D_5_m(t, r) r_1(t, r) r_2(t, r) r_3(t, r) r_4(t, r) r_5(t, r) k_c_1(t) k_c_2(t) k_c_3(t) k_c_4(t) k_c_5(t)
+@variables C_c_1(..) C_c_2(..) C_c_3(..) C_c_4(..) C_c_5(..) D_1_m(t, r) D_2_m(t, r) D_3_m(t, r) D_4_m(t, r) D_5_m(t, r) r_1(t, r) r_2(t, r) r_3(t, r) r_4(t, r) r_5(t, r) k_c_1(t) k_c_2(t) k_c_3(t) k_c_4(t) k_c_5(t)
 
 C_c_i = [C_c_1(t, r), C_c_2(t, r), C_c_3(t, r), C_c_4(t, r), C_c_5(t, r)]
 C_c_i_rad = [C_c_1(t, rad_cat), C_c_2(t, rad_cat), C_c_3(t, rad_cat), C_c_4(t, rad_cat), C_c_5(t, rad_cat)]
@@ -269,13 +269,13 @@ expand_Dr_D_im = expand_derivatives.(Dr_D_im)
 
 using ModelingToolkit: scalarize
 
-eqs_Pc = [P_c ~ (C_c_i[1] + C_c_i[2] + C_c_i[3] + C_c_i[4] + C_c_i[5]) * R * T_c]
+#eqs_Pc = [P_c ~ (C_c_i[1] + C_c_i[2] + C_c_i[3] + C_c_i[4] + C_c_i[5]) * R * T_c]
 eqs_Dim = [scalarize(D_i_m .~ D_i_m_func(C_c_i, θ, τ, T_c, P_c))...]
 eqs_ri = [scalarize(r_i .~ r_i_func(C_c_i, d_cat, θ, P_c, T_c))...]
 eqs_kci = [scalarize(k_c_i .~ k_c_i_func(T_c, P_c, R, C_i, D_i_m))...]
 DE4 = [Dt(C_c_i[i]) ~ (((2 * D_i_m[i]) / rad) + expand_Dr_D_im[i]) * Dr(C_c_i[i]) + D_i_m[i] * Drr(C_c_i[i]) + r_i[i] for i in 1:5]
 
-eqs = [eqs_Pc...; eqs_Dim...; eqs_ri...; eqs_kci...; DE4...]
+eqs = [eqs_Dim...; eqs_ri...; eqs_kci...; DE4...]
 
 ICS_C_c_i = [C_c_1(0.0, r) ~ C_c_i_init[1], C_c_2(0.0, r) ~ C_c_i_init[2], C_c_3(0.0, r) ~ C_c_i_init[3], C_c_4(0.0, r) ~ C_c_i_init[4], C_c_5(0.0, r) ~ C_c_i_init[5]]
 BCS2 = [Dr(C_c_1(t, 0.0)) ~ 0.0, Dr(C_c_2(t, 0.0)) ~ 0.0, Dr(C_c_3(t, 0.0)) ~ 0.0, Dr(C_c_4(t, 0.0)) ~ 0.0, Dr(C_c_5(t, 0.0)) ~ 0.0]
@@ -285,15 +285,13 @@ bcs = [ICS_C_c_i...; BCS2...; BCS3...]
 
 using OrdinaryDiffEq, DomainSets, MethodOfLines
 
-
-
 # Domain
 domains = [t ∈ Interval(0.0, 1.0),
     r ∈ Interval(0.0, rad_cat)]
 
 # System
-vars = [C_c_1(t, r), C_c_2(t, r), C_c_3(t, r), C_c_4(t, r), C_c_5(t, r), P_c, D_1_m, D_2_m, D_3_m, D_4_m, D_5_m, r_1, r_2, r_3, r_4, r_5, k_c_1, k_c_2, k_c_3, k_c_4, k_c_5]
-params_scal = [T_c => T_c_val, R => R_atmm3, θ => θ_val, τ => τ_val, rad => rad_cat, d_cat => d_cat_val]
+vars = [C_c_1(t, r), C_c_2(t, r), C_c_3(t, r), C_c_4(t, r), C_c_5(t, r), D_1_m, D_2_m, D_3_m, D_4_m, D_5_m, r_1, r_2, r_3, r_4, r_5, k_c_1, k_c_2, k_c_3, k_c_4, k_c_5]
+params_scal = [T_c => T_c_val, R => R_atmm3, θ => θ_val, τ => τ_val, rad => rad_cat, d_cat => d_cat_val, P_c => P_c_val]
 params_vec = [C_i[i] => C_i_val[i] for i in 1:5]
 params = [params_scal...; params_vec...]
 @named WGS_pde = PDESystem(eqs, bcs, domains, [t, r], vars, params)
@@ -308,3 +306,11 @@ prob = discretize(WGS_pde, discretization)
 sol = solve(prob, FBDF(), saveat = 0.001, abstol = 1e-6, reltol = 1e-6)
 sols = sol[C_c_1(t, r)]
 plot(sols[])
+
+#### Particle balance for diffusion within a sphere ####
+
+### Listing some assumptions made for this part of the code ###
+# 1. constant diffusivity
+# 2. no Reaction
+# 3. constant temperature and pressure
+
